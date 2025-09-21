e4a98a015c97601aa2361a2fd40a6bcbb49c0c61
Shahan S Shetty <shahansshetty@gmail.com>
2025-09-21 14:21:56 +0530

    train and test files

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 from stable_baselines3 import PPO
-from falcon9_env import Falcon9LandingEnv
+from lander_env import Falcon9LandingEnv
 
 # Load your trained model
-model = PPO.load("falcon9_landing_ppo")
+model = PPO.load("PPO_with_5_AP_2.zip")
 # Create environment with rendering to watch
 env = Falcon9LandingEnv(render_mode="human")
 
@@ -13,13 +13,13 @@ highest=-999
 l_count,count=0,0
 
 for step in range(1000):
-    action, _states = model.predict(obs, deterministic=True)
+    action, _states = model.predict(obs)
     obs, reward, terminated, truncated, info = env.step(action)
     
     if terminated or truncated:
         if info["landed_successfully"]:
-            print(f"🏁 Landing completed!")
-            
+            print(f"Landing completed!")
+            break
         else:
             print('crashed')
         print(f"Distance from target: {info['distance_to_target'] :.2f}m")
--- train.py
+++ train.py
@@ -1,33 +1,32 @@
 from stable_baselines3 import PPO
-from falcon9_env import Falcon9LandingEnv
+from lander_env import Falcon9LandingEnv
 import torch
 import time
 # Your training code
-def main():
-    if torch.cuda.is_available():
-        print("Using GPU !!!!!")
-        print(f"Device Name: {torch.cuda.get_device_name(0)}")
-    else:
-        print(" error :( . no gpu")
-        return
-    env = Falcon9LandingEnv(render_mode=None)
-    model = PPO("MlpPolicy",env,verbose=1,tensorboard_log="./logs/", device="auto")
-    
-    model.learn(total_timesteps=1000000,progress_bar=True,tb_log_name="ppo_model_7.1_3")
-    model.save('ppo_changed_metrics_1M_timesteps')
-    print('model done .')
-    env.close()
 
-if __name__=="__main__":
-    main()
+# if torch.cuda.is_available():
+#     print("Using GPU !!!!!")
+#     print(f"Device Name: {torch.cuda.get_device_name(0)}")
+# else:
+#     print(" error :( . no gpu")
+    # return
 
 
 
-# model=PPO.load('updated_reward_model_v2.zip',env,verbose=1,tensorboard_log="./logs/",device="cuda")
-# model.learn(total_timesteps=100000, reset_num_timesteps=False, progress_bar=True,tb_log_name="ppo_model_6.9_2")
+env = Falcon9LandingEnv(render_mode=None)
+model = PPO("MlpPolicy",env,tensorboard_log="./logs/"
+)
+model = PPO("MlpPolicy",env,verbose=1,tensorboard_log="./logs/",learning_rate=0.00003,ent_coef=0.01 )
+model.learn(total_timesteps=1000000,progress_bar=True,tb_log_name="PPO_with_5_AP_5",)
+model.save('PPO_with_5_AP_5')
+
+# model=PPO.load('PPO_with_5_AP_2.zip',env,verbose=1,tensorboard_log="./logs/")
+# model.learn(total_timesteps=1000000, reset_num_timesteps=False, progress_bar=True,tb_log_name="PPO_with_5_AP_2")
 # print(" Model saved!")
-# model.save("updated_reward_model_v2.zip")
+# model.save("PPO_with_5_AP_2.zip")
 
+print('model done .')
+env.close()
 
 
 
