f39483d1f60a4d5c182e8542bc21853f57558cd4
Shahan S Shetty <shahansshetty@gmail.com>
2025-09-23 12:37:38 +0530

    training multiple environments using SubprocVecEnv (cuts the training time by a lot)

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ from stable_baselines3 import PPO
 from lander_env import Falcon9LandingEnv
 
 # Load your trained model
-model = PPO.load("PPO_with_5_AP_2.zip")
+model = PPO.load("PPO_with_5_AP_7.zip")
 # Create environment with rendering to watch
-env = Falcon9LandingEnv(render_mode="human")
+env = Falcon9LandingEnv(render_mode="rgb_array")
 
 # Use the model
 obs, info = env.reset()
@@ -12,26 +12,25 @@ obs, info = env.reset()
 highest=-999
 l_count,count=0,0
 
-for step in range(1000):
+for step in range(50000):
     action, _states = model.predict(obs)
     obs, reward, terminated, truncated, info = env.step(action)
     
     if terminated or truncated:
         if info["landed_successfully"]:
             print(f"Landing completed!")
-            break
+            l_count+=1
+            # break
         else:
             print('crashed')
         print(f"Distance from target: {info['distance_to_target'] :.2f}m")
         print(f"Landing speed: {info['speed']:.2f}m/s")
         count+=1
         obs, info = env.reset()
-
     if reward>highest:
         highest=reward
 
-    if reward==100:
-        l_count+=1
+        
     # if step%200==0:
     #     print(step)
     
--- train.py
+++ train.py
@@ -1,33 +1,39 @@
 from stable_baselines3 import PPO
 from lander_env import Falcon9LandingEnv
+from stable_baselines3.common.vec_env import SubprocVecEnv
+
+#for cuda
+
 import torch
-import time
+# import time
+import sys
 # Your training code
 
-# if torch.cuda.is_available():
-#     print("Using GPU !!!!!")
-#     print(f"Device Name: {torch.cuda.get_device_name(0)}")
-# else:
-#     print(" error :( . no gpu")
-    # return
+if torch.cuda.is_available():
+    print("Using GPU !!!!!")
+    print(f"Device Name: {torch.cuda.get_device_name(0)}")
+else:
+    print(" error :( . no gpu")
+    sys.exit()
+
+def make_env():
+    return Falcon9LandingEnv(render_mode=None)
 
 
 
-env = Falcon9LandingEnv(render_mode=None)
-model = PPO("MlpPolicy",env,tensorboard_log="./logs/"
-)
-model = PPO("MlpPolicy",env,verbose=1,tensorboard_log="./logs/",learning_rate=0.00003,ent_coef=0.01 )
-model.learn(total_timesteps=1000000,progress_bar=True,tb_log_name="PPO_with_5_AP_5",)
-model.save('PPO_with_5_AP_5')
+# 
 
-# model=PPO.load('PPO_with_5_AP_2.zip',env,verbose=1,tensorboard_log="./logs/")
-# model.learn(total_timesteps=1000000, reset_num_timesteps=False, progress_bar=True,tb_log_name="PPO_with_5_AP_2")
+# model=PPO.load('PPO_with_5_AP_6.zip',env,verbose=1,tensorboard_log="./new_logs/")
+# model.learn(total_timesteps=500000, reset_num_timesteps=False, progress_bar=True,tb_log_name="PPO_with_5_AP_6")
 # print(" Model saved!")
-# model.save("PPO_with_5_AP_2.zip")
+# model.save("PPO_with_5_AP_6-2.zip")
 
-print('model done .')
-env.close()
+# print('model done .')
+# env.close()
 
-
-
-
+if __name__=='__main__':
+    env = SubprocVecEnv([make_env for _ in range(8)])
+    model = PPO("MlpPolicy",env,tensorboard_log="./new_logs/",device='cuda',batch_size=128,n_steps=512)
+    # model = PPO("MlpPolicy",env,verbose=1,tensorboard_log="./logs/",learning_rate=0.00003,ent_coef=0.01 )
+    model.learn(total_timesteps=1000000,progress_bar=True,tb_log_name="PPO_with_5_AP_7",)
+    model.save('PPO_with_5_AP_7')
